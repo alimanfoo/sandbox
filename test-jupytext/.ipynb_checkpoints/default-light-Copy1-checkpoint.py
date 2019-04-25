a61b7b8ae1c762fbbca7490177ba584d13697d85
Alistair Miles <alimanfoo@googlemail.com>
2019-04-25 11:15:20 +0100

    test jupytext

---
+++ test-jupytext/.ipynb_checkpoints/default-light-Copy1-checkpoint.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+print('hello')
+
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+import numpy as np
+
+x = np.arange(100)
+plt.plot(x)
+
+
---
+++ test-jupytext/.ipynb_checkpoints/default-md-checkpoint.md
@@ -0,0 +1,22 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,md
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.1'
+      jupytext_version: 1.1.1
+  kernelspec:
+    display_name: Python 3
+    language: python
+    name: python3
+---
+
+```python
+print('hello')
+```
+
+```python
+
+```
